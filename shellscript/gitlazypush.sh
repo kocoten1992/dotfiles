ce56d2c7cb6c80421af15ae72a68657192ef1800
Le Chuong <ko_co_ten_1992@yahoo.com>
2016-11-05 04:45:16 +0700

    hopefuly finale

--- shellscript/gitlazypush.sh
+++ shellscript/gitlazypush.sh
@@ -3,12 +3,12 @@
 # Hello there user, please fill in the configuration and then we could start
 # After fill in config, turn back and read this
 # So if you want to quick n dirty git push, map an alias command to run this script
-# ex: lg fix_some_issue (take first argument as commit message, lg -> lazygit)
+# ex: ch fix_some_issue (take first argument as commit message)
 
 #config
 project_path="/home/kocoten1992/Code/someproject/website" #use absolute path
-git_user="kocoten1992" #when you create ssh-keygen for github
-git_email="ko_co_ten_1992@yahoo.com" #git config user.email
+git_user="someguy" #when you create ssh-keygen for github
+git_email="someguy@example.com" #git config user.email
 
 # From now to the end, it none of your business, feel free to read though
 # Logging into the user
@@ -29,7 +29,7 @@ if [ $current_user = "root" ]; then
 
   chown $git_user -R $project_path
 
-  chmod 755 -R . $project_path
+  chmod 755 -R $project_path
 
   exec sudo -u $git_user /bin/sh - << _EOF_
 
